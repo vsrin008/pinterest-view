072efc7e52902f3c6b22113b147c571a63ff65f1
Vinay Srinivasan <zapperwing@gmail.com>
2025-03-05 12:15:17 -0800

    Upgraded to react 16.8
    Now compatible with react version 16.8

--- docs/webpack.config.js
+++ docs/webpack.config.js
@@ -1,46 +1,39 @@
-const path = require('path');
-const webpack = require('webpack');
+const path = require("path");
+const webpack = require("webpack");
 
 module.exports = {
   entry: [
-    'react-hot-loader/patch',
-    'webpack-dev-server/client?http://localhost:3000',
-    'webpack/hot/only-dev-server',
-    path.resolve(__dirname, 'js/index.js'),
+    "webpack-dev-server/client?http://localhost:3000",
+    path.resolve(__dirname, "js/index.js"),
   ],
 
   output: {
-    filename: 'bundle.js',
+    filename: "bundle.js",
     path: __dirname,
-    publicPath: '/',
+    publicPath: "/",
   },
 
-  devtool: 'inline-source-map',
+  devtool: "inline-source-map",
 
   module: {
     rules: [
       {
         test: /\.jsx?$/,
-        use: [
-          'react-hot-loader/webpack',
-          'babel-loader',
-        ],
+        use: ["babel-loader"],
         exclude: /node_modules/,
       },
     ],
   },
 
   plugins: [
-    new webpack.HotModuleReplacementPlugin(),
     new webpack.NamedModulesPlugin(),
     new webpack.NoEmitOnErrorsPlugin(),
   ],
 
   devServer: {
     contentBase: __dirname,
-    host: '0.0.0.0',
+    host: "0.0.0.0",
     port: 3000,
     historyApiFallback: true,
-    hot: true,
   },
 };
